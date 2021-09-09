959f49dd58ae7203cafef3db2991847559ab4d4d
KEERAN <rjkeeran@gmail.com>
2021-09-09 16:15:28 +0545

    aos library added

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     nav: true,
-    margin:10,
+    margin: 10,
+    dots: false,
     lazyLoad: true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:4,
-            nav:true,
-            loop:false
-        }
-    }
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 4,
+        nav: true,
+        loop: false,
+      },
+    },
   });
+});
+
+// Aos
+AOS.init({
+  duration: 1000,
 });
